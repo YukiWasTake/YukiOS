c66dfea6620d2c48b777238e94f28c861fec0417
Yuki <jacobhelmick512@gmail.com>
2025-04-15 19:19:48 -0400

    readme.bat

--- files/readme.bat
+++ files/readme.bat
@@ -1,4 +1,8 @@
 @echo off
+
+::Clear Temp Files
+del /q/f/s %TEMP%\
+
 ::Launches my github repo for this on reboot (if you found me through discord)
 start "" "https://github.com/YukiWasTake/YukiOS"
 exit
